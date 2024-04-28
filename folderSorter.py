0461a17dc600c38c9d75965f79a7d53bdf7e2261
devidodu55 <145554511+devidodu55@users.noreply.github.com>
2024-04-28 10:32:19 +0200

    Create folderSorter.py

---
+++ folderSorter.py
@@ -0,0 +1,48 @@
+import os
+import shutil
+
+
+def folder():
+    os.chdir("C:/Your/path/to/your/folder/to/sort")
+
+    os.mkdir('videos')
+    os.mkdir('apps')
+    os.mkdir('musics')
+    os.mkdir('images')
+    os.mkdir('PDF')
+    os.mkdir('random')
+    os.mkdir('documents')
+    os.mkdir('ZIP')
+
+
+def Orga():
+    for file in os.listdir():
+        if file.endswith('.mp4') or file.endswith('.avi'):
+            shutil.move(file, 'videos')
+
+        elif file.endswith('.png') or file.endswith('.jpg') or file.endswith('.jpeg') or file.endswith('.gif'):
+            shutil.move(file, 'images')
+
+        elif file.endswith('.doc') or file.endswith('.docx') or file.endswith('.txt'):
+            shutil.move(file, 'documents')
+
+        elif file.endswith('.zip') or file.endswith('.rar') or file.endswith('.7z'):
+            shutil.move(file, 'ZIP')
+
+        elif file.endswith('.exe') or file.endswith('.msi'):
+            shutil.move(file, 'apps')
+
+        elif file.endswith('.mp3') or file.endswith('.wav'):
+            shutil.move(file, 'musics')
+
+        elif file.endswith('.pdf'):
+            shutil.move(file, 'PDF')
+
+
+
+
+folder()
+Orga()
+
+
+
